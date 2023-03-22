d3b2170d9bdc2ad507c560459c9ebb7563e445f5
seon-2 <phia130@naver.com>
2023-03-22 17:57:40 +0900

    ✨ 시계 기능

--- startscreen/main.js
+++ startscreen/main.js
@@ -1,16 +1,33 @@
-const time = document.querySelector(".time");
-// html의 time이라는 class와 연결. document라는 object의 querySelector 함수
-// . 붙이면 class 접근, #은 id로 접근, 안 붙이면 태그명으로 접근
-// console.log(time); // 확인
+// 함수로 만들기
+function getTime() {
+  const time = document.querySelector(".time");
+  // html의 time이라는 class와 연결. document라는 object의 querySelector 함수
+  // . 붙이면 class 접근, #은 id로 접근, 안 붙이면 태그명으로 접근
+  // console.log(time); // 확인
 
-const newDate = new Date();
-// 현재 시간 정보 가져와서 newDate 변수에 저장
+  const newDate = new Date();
+  // 현재 시간 정보 가져와서 newDate 변수에 저장
 
-// 시, 분, 초 정보만 따로 저장
-const hours = newDate.getHours();
-const minutes = newDate.getMinutes();
-const seconds = newDate.getSeconds();
+  // 시, 분, 초 정보만 따로 저장
+  //   1:1:1 -> 01:01:01 으로 화면에 출력
+  const hours = String(newDate.getHours()).padStart(2, "0");
+  const minutes = String(newDate.getMinutes()).padStart(2, "0");
+  let seconds = String(newDate.getSeconds()).padStart(2, "0");
 
-time.innerText = hours + ":" + minutes + ":" + seconds;
-// innerText : text를 변경하는 함수
-// 시간이 흐르지 않음. 새로고침 했을 때만 변경됨, 필요없는 정보들도 있음
+  // 1:1:1 -> 01:01:01 으로 화면에 출력하는 다른 방법
+  //   if (seconds.toString().length === 1) {
+  //     seconds = "0" + seconds;
+  //   }
+
+  // 변수와 문자열을 함께 출력
+  //   time.innerText = hours + ":" + minutes + ":" + seconds;
+  time.innerText = `${hours} : ${minutes} : ${seconds}`;
+  // innerText : text를 변경하는 함수
+}
+
+// 함수 실행
+// getTime();
+
+// 1초 마다 실행할 수 있도록
+// setInterval(실행할 함수, 얼만큼 반복) 1000 = 1초
+setInterval(getTime, 1000);
