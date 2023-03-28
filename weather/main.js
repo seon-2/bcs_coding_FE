0aa1924ed2472b866553b6e6446124ddd9e7cd44
seon-2 <phia130@naver.com>
2023-03-28 14:39:15 +0900

    위치 기반 지역, 온도 출력

--- weather/main.js
+++ weather/main.js
@@ -1,14 +1,20 @@
 const API_KEY = "678eedb9316b9f801bce5335de3ff6f1";
 
+const weatherIcon = document.querySelector(".weatherIcon");
+const weatherTemp = document.querySelector(".weatherTemp");
+
 navigator.geolocation.getCurrentPosition(
   (position) => {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
 
     fetch(url)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        weatherTemp.innerText =
+          data.name + ", " + parseInt(data.main.temp) + "℃";
+      });
   },
   () => {
     alert("Not allowed");
